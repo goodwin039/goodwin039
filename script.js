8a20499c89bd0fa612885dddb8061edf31b28af9
Andrew Ermolaev <goodwin039@gmail.com>
2023-07-14 13:01:24 +0300

    Slider2 14-07-2023

--- script.js
+++ script.js
@@ -21,24 +21,41 @@ rightArr.addEventListener('click', () => {
 });
 
 admiral.addEventListener('click', () => {
+  admiral.classList.remove('active_grey');
+  admiral.classList.add('active_yellow');
+  thieves.classList.add('active_grey');
+  patriotic.classList.add('active_grey');
   slideIndex = 0;
   slide();
 });
 
 thieves.addEventListener('click', () => {
+  thieves.classList.remove('active_grey');  
+  thieves.classList.add('active_yellow');
+  admiral.classList.add('active_grey');
+  patriotic.classList.add('active_grey');
   slideIndex = 1;
   slide();
 });
 
 patriotic.addEventListener('click', () => {
+  // patriotic.style.color = '#E3B873';
+  // patriotic.style.borderBottom = '1px solid #E3B873';
+  // admiral.style.color = 'rgba(255, 255, 255, 0.3)';
+  // admiral.style.borderBottom = 'none';
+  patriotic.classList.remove('active_grey');
+  patriotic.classList.add('active_yellow');
+  admiral.classList.add('active_grey');
+  thieves.classList.add('active_grey');
   slideIndex = 2;
-  slide();
+  slide();  
 });
 
 const slide = () => {
   const imageWidth = slider.clientWidth;
   const slideOffset = -slideIndex * imageWidth;
   slider.style.transform = `translateX(${slideOffset}px)`;
+  
 }
 
 window.addEventListener('load', () => {
